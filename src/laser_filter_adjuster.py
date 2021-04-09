da23f5239ebc3a345bef670a1a07af28ed34498f
Eric Wiener <ericwiener3@gmail.com>
2021-04-08 21:12:19 -0400

    FEAT: updated laser_filter_adjuster to use DynamicReconfigure
    Made a PR to laser filters to make this work:
    https://github.com/ros-perception/laser_filters/pull/111

--- src/laser_filter_adjuster.py
+++ src/laser_filter_adjuster.py
@@ -1,46 +1,29 @@
-import rospy
+import dynamic_reconfigure.client
 
 
 class LaserFilterAdjuster:
     def __init__(self):
-        self.FRONT_NAMESPACE = "/laser_filter_front/scan_filter_chain/"
-        self.REAR_NAMESPACE = "/laser_filter_rear/scan_filter_chain/"
-
-        """
-        >>> rospy.get_param("/laser_filter_front/scan_filter_chain/")
-            [
-                {
-                    'type': 'laser_filters/LaserScanRangeFilter',
-                    'params':
-                        {'upper_replacement_value': inf,
-                        'use_message_range_limits': False,
-                        'upper_threshold': 2.0,
-                        'lower_threshold': 0.0,
-                        'lower_replacement_value': -inf
-                    },
-                    'name': 'range'
-                }
-            ]
-
-        """
-        self.front_params = rospy.get_param(self.FRONT_NAMESPACE)
-        self.rear_params = rospy.get_param(self.REAR_NAMESPACE)
+        self.front_client = dynamic_reconfigure.client.Client("/laser_filter_front/range/")
+        self.rear_client = dynamic_reconfigure.client.Client("/laser_filter_rear/range/")
+        self.front_params = self.front_client.get_configuration()
+        self.rear_params = self.rear_client.get_configuration()
 
         print("LaserFilter front params", self.front_params)
         print("LaserFilter rear params", self.rear_params)
 
     def front_upper_threshold(self):
-        return self.front_params[0]["params"]["upper_threshold"]
+        return self.front_params["upper_threshold"]
 
     def rear_upper_threshold(self):
-        return self.front_params[0]["params"]["upper_threshold"]
+        return self.front_params["upper_threshold"]
 
-    def increase_radius(self, amount=0.2):
-        self.front_params[0]["params"]["upper_threshold"] += amount
-        self.rear_params[0]["params"]["upper_threshold"] += amount
+    def increase_radius(self, amount=1.0):
+        self.front_params = self.front_client.update_configuration({
+            "upper_threshold": self.front_upper_threshold() + amount
+        })
+        self.rear_params = self.rear_client.update_configuration({
+            "upper_threshold": self.rear_upper_threshold() + amount
+        })
 
-        rospy.set_param(self.FRONT_NAMESPACE, self.front_params)
-        rospy.set_param(self.REAR_NAMESPACE, self.rear_params)
-
-    def decrease_radius(self, amount=0.2):
+    def decrease_radius(self, amount=1.0):
         return self.increase_radius(-1 * amount)
